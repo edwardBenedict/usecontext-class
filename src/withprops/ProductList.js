ee4f76919172696e2d7da87f72d5416cf52a9838
edwardBenedict <edwardbenedict92@gmail.com>
2022-04-09 14:36:35 +0200

    v2

--- src/withprops/ProductList.js
+++ src/withprops/ProductList.js
@@ -1,8 +1,19 @@
 import React from "react";
+import Product from "./Product";
 
-const ProductList = ({ products }) => {
-  console.log(products);
-  return <div>ProductList</div>;
+const ProductList = ({ products, increasePrice }) => {
+  
+  return (
+    <div>
+      {products.map((product) => (
+        <Product
+          key={product.id}
+          product={product}
+          increasePrice={increasePrice}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default ProductList;
--- src/withprops/WithPropsApp.js
+++ src/withprops/WithPropsApp.js
@@ -1,19 +1,24 @@
-import {useState} from "react";
+import { useState } from "react";
 import ProductList from "./ProductList";
 
 const initialProducts = [
-    { product: "Basketball", price: 12.99 },
-    { product: "Baseball", price: 9.99 },
-    { product: "Tennis Racquet", price: 89.99 }
-]
+  { id: 1, product: "Basketball", price: 12 },
+  { id: 2, product: "Baseball", price: 9 },
+  { id: 3, product: "Tennis Racquet", price: 89 },
+];
 
 const WithPropsApp = () => {
-    const [products, setProducts] = useState(initialProducts)
-    
+  const [products, setProducts] = useState(initialProducts);
+
+  const increasePrice = () => {
+    // const newProducts = [...products];
+    console.log('increasePrice')
+  };
+
   return (
     <div>
       <h3>WithPropsApp</h3>
-      <ProductList products={products}/>
+      <ProductList products={products} increasePrice={increasePrice} />
     </div>
   );
 };
